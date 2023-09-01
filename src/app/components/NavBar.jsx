45502895a40b0e0ec790a3b506243be5b326bc92
Man.D.Klaus <mansilland@gmail.com>
2023-09-01 03:38:15 -0300

    Corrección en NavBar y Scroll del contenido

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -13,15 +13,15 @@ import Sun from "../../../public/img/Sun.png"
 
 function NavBar({ falseFull, showDark, dark }) {
     return (
-        <div className={`bg-gradient-to-t ${!dark && "from-slate-950 via-slate-600 to-slate-500"} dark:from-gray-300 dark:via-gray-200 dark:to-white dark:text-gray-950 w-full`} >
+        <div className={`bg-gradient-to-t ${!dark && "from-slate-950 via-slate-600 to-slate-500"} dark:from-gray-300 dark:via-gray-200 dark:to-white dark:text-gray-950 h-full w-full p-[calc(100vh/10)]`} >
 
             <button className="absolute top-0 right-0 w-16 h-16 z-50 flex items-center justify-center" onClick={showDark}>
                 <Image width="25" height="25" src={dark ? Moon : Sun} alt="Mode" />
             </button>
 
-            <ul className="absolute right-0 h-full w-16 text-gray-300 flex flex-col justify-center" >
+            <ul className="absolute right-0 h-4/5 w-16 text-gray-300 flex flex-col justify-center" >
 
-                <li className='h-32 w-16 shadow-md z-40'>
+                <li className='h-1/4 w-16 shadow-md z-40'>
                     <a href="#About" className='h-full w-full flex items-center justify-center' onClick={falseFull}>
                         <div className="w-6 h-6">
                             <Image width="25" height="25" src={dark ? About : AboutW} alt="Link about me" />
@@ -30,7 +30,7 @@ function NavBar({ falseFull, showDark, dark }) {
                 </li>
 
 
-                <li className='h-32 w-16 shadow-md z-30' >
+                <li className='h-1/4 w-16 shadow-md z-30' >
                     <a href="#Projects" className='h-full w-full flex items-center justify-center' onClick={falseFull}>
                         <div className="w-6 h-6" >
                             <Image width="25" height="25" src={dark ? Projects : ProjectsW} alt="Link projects" />
@@ -38,7 +38,7 @@ function NavBar({ falseFull, showDark, dark }) {
                     </a>
                 </li>
 
-                <li className='h-32 w-16 shadow-md z-20' >
+                <li className='h-1/4 w-16 shadow-md z-20' >
                     <a href="#Technologies" className='h-full w-full flex items-center justify-center' onClick={falseFull}>
                         <div className="w-6 h-6" >
                             <Image width="25" height="25" src={dark ? Tecnologies : TecnologiesW} alt="Link tecnologies" />
@@ -46,7 +46,7 @@ function NavBar({ falseFull, showDark, dark }) {
                     </a>
                 </li>
 
-                <li className='h-32 w-16 shadow-md z-10' >
+                <li className='h-1/4 w-16 shadow-md z-10' >
                     <a href="#Contact" className='h-full w-full flex items-center justify-center' onClick={falseFull}>
                         <div className="w-6 h-6" >
                             <Image width="25" height="25" src={dark ? Contact : ContactW} alt="Link contact" />
